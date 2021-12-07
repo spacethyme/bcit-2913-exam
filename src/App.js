3f61b19fe5d149cb4d1a1d84a922d2d63bccad05
space thyme <spacethyme@shaw.ca>
2021-12-06 18:37:11 -0800

    add data file; import and test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import books from './data.js';
 
 function App() {
+  console.log(books);
   return (
     <div className="App">
       <p>hello world</p>
+      <p>{JSON.stringify(books)}</p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const books = [
+    {
+      id: 0,
+      name: "Harry Potter and the Sorcerer's Stone",
+      author: "JK Rowling",
+      coverImg:
+        "https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg",
+      sequels: ["Harry Potter and the Chamber of Secrets",
+   "Harry Potter and the Prisoner of Azkaban",
+   "Harry Potter and the Goblet of Fire",
+  "Harry Potter and the Order of the Phoenix",
+   "Harry Potter and the Half-Blood Prince", 
+  "Harry Potter and the Deathly Hallows"],
+      rating: 3
+    },
+  
+    {
+      id: 1,
+      name: "The pragmatic programmer",
+      author: "Andrew Hunt",
+      coAuthor: "David Thomas",
+      coverImg:
+        "https://images-na.ssl-images-amazon.com/images/I/41HXiIojloL.jpg",
+     rating: 4,
+    },
+    {
+      id: 2,
+      name: "Cracking the Coding Interview",
+      author: "Gayle Laakmann",
+      coverImg:
+        "https://images-na.ssl-images-amazon.com/images/I/619M-4xNINL.jpg",
+      rating: 5
+    },
+    {
+      id: 3,
+      name: "Blink",
+      author: "Malcolm Gladwell",
+      coverImg:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1440763417l/40102._SX318_.jpg",
+      rating: 4
+    }
+  ];
+  
+  export default books;
