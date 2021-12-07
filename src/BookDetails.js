b546504e25e7b46c96e6797f9c7e6af183d387f9
space thyme <spacethyme@shaw.ca>
2021-12-06 20:15:44 -0800

    css details

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,24 +1,9 @@
 import books from './data.js';
 
-function SequelList ({ sequels }) {
-    return (
-        <>
-            <h3>List of Sequels:</h3>
-            <ul>
-                {Object.keys(sequels)
-                    .map((key) => (
-                    <li key={key}>{sequels[key]}</li>
-                ))}
-            </ul>
-        </>
-    )
-}
-
 export default function BookDetails ({ activeBook }) {
 
     const fallbackImage = "https://via.placeholder.com/300/222222/000000?text=placeholder.com";
     const book = books[activeBook];
-    console.log(book);
 
     return (
         <section className="book-details">
@@ -34,10 +19,17 @@ export default function BookDetails ({ activeBook }) {
                         : ""
                     }
                 </ul>
-                {book.sequels ?
-                    <SequelList sequels={book.sequels} />
-                    : ""
-                }
+                <h3>List of Sequels:</h3>
+                <ul>
+                    {book.sequels ?
+                        Object.keys(book.sequels)
+                            .map((key) => (
+                            <li key={key}>{book.sequels[key]}</li>
+                        ))
+                    :
+                    <li>(no sequels)</li>
+                    }
+                </ul>
                 <h3 className="rating">Rating: {book.rating}</h3>
             </div>
         </section>
